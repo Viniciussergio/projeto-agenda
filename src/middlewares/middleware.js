2d7b58ffb3490251b0c9dcd3f69b2335c0f239ee
Vinicius Sergio <viniciussergio.so@gmail.com>
2022-01-27 16:05:07 -0300

    Correçao de endereco

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -25,7 +25,7 @@ exports.csrfMiddleware = (req, res, next) => {
 exports.loginRequired = (req, res, next) => {
   if(!req.session.user) {
     req.flash('errors', 'Você precisa fazer login.');
-    req.session.save(() => res.redirect('/'));
+    req.session.save(() => res.redirect('/agenda'));
     return;
   }
 
